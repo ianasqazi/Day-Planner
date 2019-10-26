4bab8d4e3d8af085ca877a7f569005984a8927c2
Anas Qazi <ianasqazi@gmail.com>
2019-10-25 23:01:26 -0400

    Logic for color coding according to time working

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,55 @@
-// Display Date in the Top Jumbotron 
 
 $(document).ready(function(){
 
-
-// Display Date in the Top Jumbotron 
+// Displaying the date in the Top Jumbotron 
 
     var currentDay = $("#currentDay");
     var curDate = moment().format('MMMM Do YYYY');
     currentDay.text(curDate);
 
-// creating the Time Blocks when page loads
+// Creating the Time Blocks when page loads
 
-    // for (var i = 9; i <= 17; i++){
-    //     $('.container').append(`<div class="row time-block" id=block${i}> 
-    //     <div class="col-2 hour" id=hour${i}> <p>${i}Hrs</p> </div>
-    //     <textarea class="col-8 description" id=desc${i}></textarea> 
-    //     <button class="col-2 saveBtn i far fa-save" id=btn${i}> </i></button> 
-    //     </div>`);
-    //     }
-    var schedArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+    // var schedArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+    var schedArray = ["9","10","11","12","13","14","15","16","17"];
 
     for (var i = 0; i < schedArray.length; i++){
             $('.container').append(`<div class="row time-block" id=block${schedArray[i]}> 
             <div class="col-2 hour" id=hour${schedArray[i]}> <p>${schedArray[i]}</p> </div>
-            <textarea class="col-8 description" id=desc${schedArray[i]}></textarea> 
+            <textarea class="col-8 description" id=${schedArray[i]}></textarea> 
             <button class="col-2 saveBtn i far fa-save" id=btn${schedArray[i]}></i></button> 
             </div>`);
             }
 
-    // Color Coding according to current time 
+// Color Coding according to current time hour9AM
 
+    // var currentHour = moment().format('h') ;
+    // var currentHour = moment().hour();
+    var currentHour = 14;
+
+    for (var i = 9; i <= 17; i++){
+        if (i == currentHour) {
+            $("#"+i).addClass("present");
+        } 
+        else if (i < currentHour){
+            $("#"+i).addClass("past");
+        }
+        else {
+            $("#"+i).addClass("future");
+        } 
+ }
             
+// Load the values from local Storage if exists to the respective textbox
 
-    // saving the variable from text box to local storage variable 
+    for (var i = 9; i <= 17; i++){
+        $("#"+i).val(localStorage.getItem("btn"+i));
+    } 
+
+// Save the values of the textbox to local storage when button is the button is clicked 
+
     $(".saveBtn").click(function() {
-        // var comment = $.trim($("#comment").val());
-
         var boxText = $(this.parentNode.children[1]).val();
-        console.log(boxText);
         localStorage.setItem($(this).attr("id"), boxText);
-        // localStorage.setItem($(this).attr("id"), $(this).val());
-
-        
-
-        // var closestElement = this.closest();
-        // console.log (closestElement);
-
-        // console.log($(this).find(".description textarea"));
-
-        // $(".time-block").each(function(){
-        //     $(this).find(".description textarea").val(timeTrackObject[$(this).attr("data-time")].value);
-        //   });
-
-
-        
-        // var tBdesc9 = $("#desc9").val();
-        // localStorage.setItem("taskfor9AM", tBdesc9);
       });
 
 
-
-
-//     window.onload = function(){
-//         var val = localStorage.getItem('value'); // or localStorage.value
-  
-//         if(val == null)
-//             val = "First try";
-  
-//        document.getElementById("myData").value = val;
-//   }
-
-
-    });
+});
